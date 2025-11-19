5794d9b243f3dca118c848968fd33e93309d5af0
thefresh26 <thefresh334@gmail.com>
2025-11-19 18:21:58 -0500

    mas contenido

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 from fastapi import FastAPI, Body
-
-
+from pydantic import BaseModel #se crea para utilizar los items de manera interactiva
+from typing import Optional
 app = FastAPI()
 
+
+
+class Organized(BaseModel):
+    id: Optional[int] = None
+    matematicas: float
+    español: float
+    ed_fisica: float
+    sociales: float
+    
+    
+class NotasUpdate(BaseModel):
+    id: Optional[int] = None
+    matematicas: float
+    español: float
+    ed_fisica: float
+    sociales: float
+    
 notas = [
     
     {
         'id': 1,
         'matematicas': 2.5,
         'español': 3.9,
-        'Ed fisica': 2.6,
+        'ed fisica': 2.6,
         'sociales': 2.2,
-        'Promedio': 2.8,
-        'Estado': 'reprobado'
     },
         
     {
         'id': 2,
         'matematicas': 2.5,
         'español': 3.9,
-        'Ed fisica': 4.6,
+        'ed fisica': 4.6,
         'sociales': 4.3,
-        'Promedio': 3.8,
-        'Estado': 'aprobado'
     }
+    
 ]
         
 @app.get('/Calificaciones', tags=['home'])
@@ -40,38 +54,63 @@ def getCalificacion(id: int):
     return []
     
     
-@app.get('/ca/', tags=['Notas'])
+@app.get('/Obetener_estado/', tags=['Notas'])
 def get_calificacion(Estado: str):
     
     resultado = []
     
     for calificacion in notas:
-        if calificacion['Estado'].lower() == Estado.lower():
+        if calificacion['estado'].lower() == Estado.lower():
             resultado.append(calificacion)
 
     return resultado
 
 
 
-@app.post('/notas', tags=['Notas'])
-def create_nota(
-                id: int = Body(), 
-                matematicas: float = Body(),
-                español: float = Body(),
-                Ed_fisica: float = Body(),
-                sociales: float = Body(),
-                Promedio: float = Body(),
-                Estado: str = Body()
-                ):
+@app.post('/Agregar_notas', tags=['Notas']) 
+
+def agregar(notes: Organized):
+    global promedio
+    global estado
     
-    notas.append({
-        'id': id,
-        'matematica': matematicas,
-        'Epañol': español,
-        'Ed_Fisica': Ed_fisica,
-        'Sociales': sociales,
-        'Promedio': Promedio,
-        'Estado': Estado,
-    })
+    promedio = (
+        notes.matematicas +
+        notes.español + 
+        notes.ed_fisica +
+        notes.sociales
+    )/4
+    estado = 'aprobado' if promedio >= 3.0 else 'reprobado'
     
-    return notas 
+    nueva = notes.model_dump()
+    nueva['promedio'] = promedio
+    nueva['estado'] = estado
+    
+    notas.append(nueva)
+    return notas
+
+@app.put('/update/{id}', tags=['Notas'])
+
+def update(
+    id: int, note: Organized
+):
+    
+    for nota in notas:
+        if nota['id'] == id:
+            nota['matematicas'] = note.matematicas
+            nota['español'] = note.español
+            nota['ed fisica'] = note.ed_fisica
+            nota['sociales'] = note.sociales
+    
+    return notas
+    
+    
+@app.delete('/notas/{id}', tags=['Notas'])
+def delete(
+    id: int
+):
+    
+    for nota in notas:
+        if nota['id'] == id:
+            notas.remove(nota)
+            
+    return notas
